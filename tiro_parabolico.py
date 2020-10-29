71a576646894ec2601e6b068cadd256c62dccf48
aranzanarvaez <73494800+aranzanarvaez@users.noreply.github.com>
2020-10-29 12:13:01 -0600

    Update tiro_parabolico.py

--- tiro_parabolico.py
+++ tiro_parabolico.py
@@ -14,8 +14,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x*sp + 200) / 25
+        speed.y = (y*sp + 200) / 25
 
 def inside(xy):
     "Return True if xy within screen."
